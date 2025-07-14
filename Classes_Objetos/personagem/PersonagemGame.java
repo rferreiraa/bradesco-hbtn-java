46e740c90b1614126d0649bca2768138fd484a99
Rodrigo <rferreiraa02@gmail.com>
2025-07-14 20:59:47 -0300

    personagem game

--- Classes_Objetos/personagem/PersonagemGame.java
+++ Classes_Objetos/personagem/PersonagemGame.java
@@ -1,20 +1,4 @@
-public class Program {
+public class PersonagemGame {
     int saudeAtual;
     String nome;
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getSaudeAtual() {
-        return saudeAtual;
-    }
-
-    public void setSaudeAtual(int saudeAtual) {
-        this.saudeAtual = saudeAtual;
-    }
 }
