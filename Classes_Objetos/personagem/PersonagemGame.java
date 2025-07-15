6643e01ce3c6c745c58cd4487667b6e62a090835
Rodrigo <rferreiraa02@gmail.com>
2025-07-14 21:03:15 -0300

    ajusta codigo

--- Classes_Objetos/personagem/PersonagemGame.java
+++ Classes_Objetos/personagem/PersonagemGame.java
@@ -1,20 +1,4 @@
 public class PersonagemGame {
-    public int getSaudeAtual() {
-        return saudeAtual;
-    }
-
-    public void setSaudeAtual(int saudeAtual) {
-        this.saudeAtual = saudeAtual;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public int saudeAtual;
     public String nome;
 }
