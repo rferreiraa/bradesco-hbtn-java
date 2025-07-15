78afd3f58575a0842e3ae9dab8f5a6635499bbf0
Rodrigo <rferreiraa02@gmail.com>
2025-07-14 22:46:44 -0300

    ajustes

--- Heranca/quadrado/Quadrado.java
+++ Heranca/quadrado/Quadrado.java
@@ -17,6 +17,6 @@ public class Quadrado extends Retangulo {
     @Override
     public String toString() {
         return "[Quadrado] " +
-                + lado;
+                + String.format("%.1f", lado);
     }
 }
