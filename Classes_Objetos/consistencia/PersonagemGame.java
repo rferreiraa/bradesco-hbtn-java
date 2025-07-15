151b02e790e0788119cbedb498fbb8568f6ad383
Rodrigo <rferreiraa02@gmail.com>
2025-07-14 21:26:09 -0300

    4. Consistência

---
+++ Classes_Objetos/consistencia/PersonagemGame.java
@@ -0,0 +1,52 @@
+public class PersonagemGame {
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public PersonagemGame(int saudeAtual, String nome) {
+        setSaudeAtual(saudeAtual);
+        this.nome = nome;
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        if (saudeAtual <= 0) {
+            saudeAtual = 0;
+            status = "morto";
+        }else {
+            status = "vivo";
+        }
+        if (saudeAtual > 100) {
+            saudeAtual = 100;
+        }
+        this.saudeAtual = saudeAtual;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome != null && nome != "") {
+            this.nome = nome;
+        }
+    }
+
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        int saude = getSaudeAtual() - quantidadeDeDano;
+        setSaudeAtual(saude);
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        int saude = getSaudeAtual() + quantidadeDeCura;
+        setSaudeAtual(saude);
+    }
+}
