a4b36d7916167ab1c7fbfa28b211fc7a01759e3d
Rodrigo <rferreiraa02@gmail.com>
2025-07-15 23:20:03 -0300

    0. Workflow

---
+++ Interfaces_Exercicios/workflow/atividades/ModificarStatusVideo.java
@@ -0,0 +1,8 @@
+package atividades;
+
+public class ModificarStatusVideo implements Atividade{
+    @Override
+    public void executar() {
+        System.out.print("modificando status do video\n");
+    }
+}
