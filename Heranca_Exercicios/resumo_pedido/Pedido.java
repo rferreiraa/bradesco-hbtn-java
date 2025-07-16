0cf3c1161cbdf3197ae1bfa2eaed80e5def391c0
Rodrigo <rferreiraa02@gmail.com>
2025-07-15 22:30:11 -0300

    1. Resumo Pedido

--- Heranca_Exercicios/resumo_pedido/Pedido.java
+++ Heranca_Exercicios/resumo_pedido/Pedido.java
@@ -25,20 +25,20 @@ public class Pedido {
         double valorSemDesconto = valorTotal / (1 - percentualDesconto / 100);
         double valorDesconto = valorTotal - valorSemDesconto;
 
-        System.out.print("------- RESUMO PEDIDO ------- \n");
+        System.out.print("------- RESUMO PEDIDO -------\n");
         for (ItemPedido item : itens) {
             double valorTotalProduto = item.getQuantidade() * item.getProduto().obterPrecoLiquido();
             if(item.getProduto() instanceof Livro){
-                System.out.printf("Tipo: Livro  Titulo: %s  Preco: %.2f  Quant: %d  Total: %.2f \n", item.getProduto().getTitulo(), item.getProduto().obterPrecoLiquido(), item.getQuantidade(), valorTotalProduto);
+                System.out.printf("Tipo: Livro  Titulo: %s  Preco: %.2f  Quant: %d  Total: %.2f\n", item.getProduto().getTitulo(), item.getProduto().obterPrecoLiquido(), item.getQuantidade(), valorTotalProduto);
             }else{
-                System.out.printf("Tipo: Dvd  Titulo: %s  Preco: %.2f  Quant: %d  Total: %.2f \n", item.getProduto().getTitulo(), item.getProduto().obterPrecoLiquido(), item.getQuantidade(), valorTotalProduto);
+                System.out.printf("Tipo: Dvd  Titulo: %s  Preco: %.2f  Quant: %d  Total: %.2f\n", item.getProduto().getTitulo(), item.getProduto().obterPrecoLiquido(), item.getQuantidade(), valorTotalProduto);
             }
         }
-        System.out.print("---------------------------- \n");
-        System.out.printf("DESCONTO: %.2f \n", Math.abs(valorDesconto));
-        System.out.printf("TOTAL PRODUTOS: %.2f \n", valorSemDesconto);
-        System.out.print("---------------------------- \n");
-        System.out.printf("TOTAL PEDIDO: %.2f \n", valorTotal);
-        System.out.print("---------------------------- \n");
+        System.out.print("----------------------------\n");
+        System.out.printf("DESCONTO: %.2f\n", Math.abs(valorDesconto));
+        System.out.printf("TOTAL PRODUTOS: %.2f\n", valorSemDesconto);
+        System.out.print("----------------------------\n");
+        System.out.printf("TOTAL PEDIDO: %.2f\n", valorTotal);
+        System.out.print("----------------------------\n");
     }
 }
