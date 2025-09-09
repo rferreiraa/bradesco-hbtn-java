e72d98286b8bdd84367ec7f4e258e29c2d96970f
Rodrigo <rferreiraa02@gmail.com>
2025-09-09 15:58:35 -0300

    3. Gerenciamento de Estoque com Arquivos

--- arquivos/2/CsvFileReader.java
+++ arquivos/2/CsvFileReader.java
@@ -28,8 +28,6 @@ public class CsvFileReader {
                 System.out.println("Salarial: " + dados[3]);
                 System.out.println("------------------------");
             }
-
-            System.out.println("Leitura do arquivo concluída.");
         } catch (IOException e) {
             System.out.println("Erro ao ler o arquivo: " + e.getMessage());
         }
