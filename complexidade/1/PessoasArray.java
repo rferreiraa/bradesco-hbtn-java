5c8f66f377cad55e349b7e75686cd5c062b891e9
Rodrigo <rferreiraa02@gmail.com>
2025-09-15 13:31:03 -0300

    2. Complexidade O(log(n)) – Busca Binária

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -26,7 +26,6 @@ public class PessoasArray {
             if (nomes[i].equals(nome)) {
                 System.out.println("Nome pesquisado é " + nomes[i] + " que está na posição " + i);
                 encontrado = true;
-                break;
             }
         }
 
