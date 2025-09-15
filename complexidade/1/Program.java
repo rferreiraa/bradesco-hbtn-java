45b8d5ea7b6a88e18f12023b2e7736897db4d4c4
Rodrigo <rferreiraa02@gmail.com>
2025-09-15 13:34:47 -0300

    2. Complexidade O(log(n)) – Busca Binária

--- complexidade/1/Program.java
+++ complexidade/1/Program.java
@@ -1,10 +1,14 @@
 public class Program {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
         long inicio  = System.nanoTime();
+        try {
             PessoasArray p = new PessoasArray();
             p.buscaLinear("Otavio");
             p.buscaLinear("Isabela");
             p.buscaLinear("Anderson");
+        } finally {
+            System.out.println("Tempo de execução: " + (System.nanoTime() - inicio));
+        }
     }
 }
