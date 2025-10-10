26de10a341aad7649838259f7810a14038d9d584
Rodrigo <rferreiraa02@gmail.com>
2025-10-10 10:56:35 -0300

    0. Armazém de itens

---
+++ Generics_Exercises/armazem/Armazem.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Armazem<T> implements Armazenavel<T> {
+    private Map<String, T> itens;
+
+    public Armazem() {
+        this.itens = new HashMap<>();
+    }
+
+    @Override
+    public void adicionarAoInventario(String nome, T valor) {
+        itens.put(nome, valor);
+    }
+
+    @Override
+    public T obterDoInventario(String nome) {
+        return itens.get(nome);
+    }
+}
