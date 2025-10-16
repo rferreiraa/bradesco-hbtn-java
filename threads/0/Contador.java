bbfee80ad6a1ed7cb637c24421d03a7f0516b329
Rodrigo <rferreiraa02@gmail.com>
2025-10-16 16:33:32 -0300

    Java - Streams, Collectors e Optional

---
+++ threads/0/Contador.java
@@ -0,0 +1,13 @@
+public class Contador {
+    private int count = 0;
+
+
+    public synchronized void incrementar() {
+        count++;
+    }
+
+
+    public int getContagem() {
+        return count;
+    }
+}
