62f41adfd97a22508ef2e14843afa8ff9d5f1e4e
Rodrigo <rferreiraa02@gmail.com>
2025-10-16 15:42:11 -0300

    Java - Streams, Collectors e Optional

---
+++ 0x19/pessoa_por_cargo/Pessoa.java
@@ -0,0 +1,53 @@
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Pessoa {
+    private Integer codigo;
+    private String nome;
+    private String cargo;
+    private Integer idade;
+    private double salario;
+
+    public Pessoa(Integer codigo, String nome, String cargo, Integer idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getSalarioFormatado() {
+        NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+        return nf.format(salario);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %s %s %d %s", codigo, nome, cargo, idade, getSalarioFormatado());
+    }
+}
