191eadd373bb7b3d8a6cb920ffff949ca90c9895
Rodrigo <rferreiraa02@gmail.com>
2025-10-16 16:47:24 -0300

    Java - Streams, Collectors e Optional

---
+++ threads/1/ProdutorConsumidor.java
@@ -0,0 +1,20 @@
+public class ProdutorConsumidor {
+    public static void main(String[] args) {
+        Buffer buffer = new Buffer();
+
+        Thread produtor = new Produtor(buffer);
+        Thread consumidor = new Consumidor(buffer);
+
+        produtor.start();
+        consumidor.start();
+
+        try {
+            produtor.join();
+            consumidor.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Execução finalizada.");
+    }
+}
